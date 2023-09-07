c86c8f9413d973a225944021f3b7334d5fe78f91
김지희 Kim jihee <50134759+mii2026@users.noreply.github.com>
2023-09-08 03:41:47 +0900

    Add files via upload

---
+++ GROUPBY/3.sql
@@ -0,0 +1,7 @@
+SELECT CAR_ID, case
+                when max('2022-10-16' between start_date and end_date) then '대여중'
+                else '대여 가능'
+                end as AVAILABILITY
+from car_rental_company_rental_history
+group by car_id
+order by car_id desc;
